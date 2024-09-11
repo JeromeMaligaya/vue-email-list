82a96616add386e7be14128cc3414ad540aa2698
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-11 22:52:12 +0200

    added comment of the data of apiUrl

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ createApp({
     }
   },
   methods: {
+    getRandomEmail(){
+      axios.get(this.apiUrl)
+        .then( response => console.log(response))
+      
+    }
+  },
+  mounted(){
+    this.getRandomEmail()
   }
 }).mount('#app')
 
+/*
+studio del dato
 
+data: 
+  response: "eva12@gmail.com"
+  success: true
+*/
